708aeac572556c0c6bee1dbe154102b10324974f
Titus <ltvminhtuan99@gmail.com>
2021-09-06 23:56:23 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Home from "./components/pages/Home";
 function App() {
     return (
         <>
             <Router>
                 <NavBar />
                 <Switch>
-                    <Route path="/" exact></Route>
+                    <Route path="/" exact component={Home}></Route>
                 </Switch>
             </Router>
         </>
