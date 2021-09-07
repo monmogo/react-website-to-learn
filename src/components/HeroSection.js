19db1c2d9ac6a6ed8e3586b98161a45dec21479c
Titus <ltvminhtuan99@gmail.com>
2021-09-07 23:36:45 +0700

    finish learning to reactjs

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import "./HeroSection.css";
 function HeroSection() {
     return (
         <div className="hero-container">
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
             <h1>ADDVENTURE AWAITS</h1>
             <p>What are you looking for ?</p>
             <div className="hero-btns">
